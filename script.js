4583851e29717dd3275fcab1f52bd8766f04442a
athar-1111 <tdenger75@gmail.com>
2026-02-25 01:07:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function showSection(id) {
+
+  const sections = document.querySelectorAll(".content");
+  sections.forEach(section => {
+    section.style.display = "none";
+  });
+
+  document.getElementById(id).style.display = "block";
+}
+
+showSection("hadiths");
+function showSection(sectionId) {
+
+    // اخفاء كل الاقسام
+    let sections = document.querySelectorAll(".content");
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+
+    // اظهار القسم المختار فقط
+    document.getElementById(sectionId).style.display = "block";
+}
+let count = localStorage.getItem("tasbeehCount") || 0;
+
+document.getElementById("counter").innerText = count;
+
+function tasbeeh() {
+    count++;
+    document.getElementById("counter").innerText = count;
+    localStorage.setItem("tasbeehCount", count);
+}
+
+function resetTasbeeh() {
+    count = 0;
+    document.getElementById("counter").innerText = count;
+    localStorage.setItem("tasbeehCount", count);
+}
